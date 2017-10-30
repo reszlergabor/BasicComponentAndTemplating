56cffc659d4f488250a2e5d8637b40d99a8c281b
reszlergabor <reszlerg@gmail.com>
2017-10-30 17:04:25 +0100

    * 2017. okt. 30-i Angular gyakorlatok

---
+++ src/app/event/event.component.ts
@@ -0,0 +1,21 @@
+
+import {Component} from "@angular/core";
+
+@Component({
+  selector: 'app-event',
+  templateUrl: './event.component.html',
+  styleUrls: ['./event.component.css']
+})
+
+export class EventComponent {
+  title = 'sziasztok, mi itt vagyunk';
+
+  constructor(){
+    console.log(`${this.title} itt lehet folytatni folyo szoveggel azt, amit ki szeretnek irni`);
+    this.greetings();
+  }
+
+  greetings(){
+    console.log('not implemented yet!');
+  }
+}
