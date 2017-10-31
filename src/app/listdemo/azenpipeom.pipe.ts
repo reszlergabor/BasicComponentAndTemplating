0ecdb0b1340467f78a98b2fee9a7af039d4aa2b2
reszlergabor <reszlerg@gmail.com>
2017-10-31 12:50:39 +0100

    * add image is mukodik * edit funkcionalitas
    ** custom pipe!!

--- src/app/listdemo/azenpipeom.pipe.ts
+++ src/app/listdemo/azenpipeom.pipe.ts
@@ -5,8 +5,8 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class AzenpipeomPipe implements PipeTransform {
 
-  transform(value: any, args?: any): any {
-    return null;
+  transform(value: any, times: number): any {
+    return value.repeat(times);
   }
 
 }
